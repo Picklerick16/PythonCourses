6be0c5c9a4451179f3eb3e0731d086227068254e
GuitarPickle <dan731600@gmail.com>
2023-08-03 15:01:21 +0200

    Indoor voice function assignment finished.

--- Course1/Indoor/indoor.py
+++ Course1/Indoor/indoor.py
@@ -1,1 +1,11 @@
 
+def main():
+    Call = str(input("Be quiet, what do you want to say? "))
+    loweredCall = convert_to_indoor_voice(Call)
+    print(loweredCall)
+
+def convert_to_indoor_voice(toLower):
+    loweredCall = toLower.lower()
+    return loweredCall
+
+main()
