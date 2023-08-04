1c60bacb3b96498ee376c816c5e3716ea0292ad3
GuitarPickle <dan731600@gmail.com>
2023-08-04 17:22:09 +0200

    extensions return function

---
+++ Course1/week1/file_extensions/extensions.py
@@ -0,0 +1,20 @@
+
+def main():
+    file_name = list(input("File name: ").lower())
+    print(to_media_type(file_name))
+
+def to_media_type(file):
+    if "." in file:
+        index = file.index(".")
+        file_extension = "".join(file[index+1:])
+  
+
+        match file_extension:
+            case "gif":
+                return "image/gif"
+            case "doc":
+                return "application/msword"
+            case _:
+                return "application/octet-stream"
+
+main()
