033689bcb6cc960c0545239add4ee1852ebcc327
GuitarPickle <dan731600@gmail.com>
2023-08-04 17:24:12 +0200

    comments on extensions functin file

--- Course1/week1/file_extensions/extensions.py
+++ Course1/week1/file_extensions/extensions.py
@@ -4,11 +4,14 @@ def main():
     print(to_media_type(file_name))
 
 def to_media_type(file):
+    # Check if it is an extension
     if "." in file:
+
+        # Remove the filename and extension-dot
         index = file.index(".")
         file_extension = "".join(file[index+1:])
   
-
+        # Compare the naked extensions
         match file_extension:
             case "gif":
                 return "image/gif"
