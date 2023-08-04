538bfb236f737b10f856df9e5d64b75bb04789d9
GuitarPickle <dan731600@gmail.com>
2023-08-04 15:19:04 +0200

    answer to life funtion

---
+++ Course1/week1/deep_thought/thought.py
@@ -0,0 +1,12 @@
+
+def main():
+    answer_to_everything = str(input("What is the meaning to life? ")).lower()
+    if answer_check(answer_to_everything):
+        print("yes")
+    else: 
+        print("no")
+
+def answer_check(answer):
+    return answer == "42" | "forty-two" | "forty two"
+
+main()
