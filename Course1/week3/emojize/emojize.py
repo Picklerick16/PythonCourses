f2fe4728facc3dd1be2fca91da11e347d4c447ae
GuitarPickle <dan731600@gmail.com>
2023-08-11 11:43:20 +0200

    string to emoji converter

---
+++ Course1/week3/emojize/emojize.py
@@ -0,0 +1,11 @@
+import emoji
+
+def main():
+    print(convert_to_emoji("Emoji: "))
+    
+def convert_to_emoji(emoji_string):
+    # Ask for the user to input a string to convert to emoji
+    # Convert the input string to emoji
+    return emoji.emojize(f"Python is {input(emoji_string)}")
+
+main()
