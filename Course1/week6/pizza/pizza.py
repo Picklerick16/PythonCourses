4c17a9e8a28a38e3dbcece045a0aa03465ec054d
GuitarPickle <122364187+Picklerick16@users.noreply.github.com>
2023-09-07 10:39:12 +0200

    pizza function

--- Course1/week6/pizza/pizza.py
+++ Course1/week6/pizza/pizza.py
@@ -1,21 +1,19 @@
 import sys
+from tabulate import tabulate
+import csv
 
 def main():
-    n_lines = 0
+    menu = []
 
     # Check if there is only 1 python file as an argument
-    if len(sys.argv) > 1 and sys.argv[1][-3:] == ".py":
+    if len(sys.argv) == 2 and sys.argv[1][-4:] == ".csv":
         try:
-            with open(sys.argv[1], 'r') as file:
-                lines = file.readlines()
-                
-                for line in lines:
-                    # Remove leading and trailing whitespace
-                    line = line.strip()
+            with open(sys.argv[1]) as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    menu.append
                     
-                    # Ignore commented lines
-                    if line and not line.startswith("#"):
-                        n_lines += 1
+                   
 
             print(f"{n_lines} non-commented lines")
         except FileNotFoundError:
