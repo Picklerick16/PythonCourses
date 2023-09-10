d458532cd288bf7b29246fcb7fb34c3109b57419
GuitarPickle <dan731600@gmail.com>
2023-09-10 11:51:05 +0200

    PIL and CSV

--- Course1/week6/shirt/shirt.py
+++ Course1/week6/shirt/shirt.py
@@ -1,33 +1,35 @@
 import sys
 from PIL import Image, ImageOps
+
 def main():
     check_command_line_args()
 
     try:
-        # open shirt image
-        blank_shirt = Image.open("shirt.png")
-        size = blank_shirt.size
-        # Open to format image
-        # Resize image
-        #crop image
-        resized_image = ImageOps.fit(Image.open(sys.argv[1]), size)
-        # merge the to / overlay and make new file out of it
-        resized_image.paste(blank_shirt, blank_shirt)
-    
-        resized_image.save(sys.argv[2])
+        # Open the person image (JPEG)
+        person_image = Image.open(sys.argv[1])
+
+        # Open the shirt image (PNG with transparency)
+        shirt_image = Image.open("shirt.png")
+
+        # Resize the shirt image to fit the person's dimensions
+        shirt_image = shirt_image.resize(person_image.size)
+
+        # Overlay the shirt image on the person image
+        result_image = Image.alpha_composite(person_image.convert("RGBA"), shirt_image.convert("RGBA"))
+
+        # Save the resulting image as a JPEG
+        result_image.convert("RGB").save(sys.argv[2], "JPEG")
     except FileNotFoundError:
-        sys.exit("file not found")
-
-
+        sys.exit("File not found")
 
 
 
 def check_command_line_args():
     # Check amount of arguments
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 3:
         sys.exit("too many arguments")
     
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         sys.exit("too few arguments")
 
     # Check if file extensions are valid
@@ -39,3 +41,5 @@ def check_command_line_args():
     if not sys.argv[1][-4:] == sys.argv[2][-4:]:
         sys.exit("Files need to be of the same type")
     
+if __name__ == "__main__":
+    main()
